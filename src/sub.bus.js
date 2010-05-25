a054318d74e2583d9327a45daa226467ac5593d4
Bernardo Heynemann <heynemann@gmail.com>
2010-05-25 17:52:30 -0300

    Contributed by JCFigueiredo

--- src/sub.bus.js
+++ src/sub.bus.js
@@ -1,24 +1,46 @@
-jQuery(function(){
-    $(document).data("subs", []);
-});
+(function($){
 
-jQuery.sub = {
-    reset:function(){
-        $(document).data("subs", []);
-    },
-    getSubscriptions:function(){
-        return $(document).data("subs");
-    },
-    subscribe:function(event, callback) {
-        jQuery.sub.getSubscriptions().push({key: event, callback: callback})
-    },
-    publish:function(event, obj){
-        subs = jQuery.sub.getSubscriptions();
-        for (i=0; i<subs.length; i++){
-            subscription = subs[i];
-            if (subscription.key == event){
-                subscription.callback(obj);
+    $(document).data("subs", []);
+
+    $.sub = {
+        options : { debug : false },
+        reset:function(){
+            this._debug('resetting');
+            $(document).data("subs", []);
+        },
+        getSubscriptions:function(){
+            subscriptions = $(document).data("subs");
+            this._debug({subscriptions:subscriptions});
+            return subscriptions;
+        },
+        subscribe:function(event, callback) {
+            this._debug({'subscribing' : event, callback : callback});
+            jQuery.sub.getSubscriptions().push({key: event, callback: callback})
+        },
+        publish:function(event, obj){
+            this._debug({'publishing' : event, message : obj});
+            subs = jQuery.sub.getSubscriptions();
+            for (i=0; i<subs.length; i++){
+                subscription = subs[i];
+                if (subscription.key == event){
+                    subscription.callback(obj);
+                }
+            }
+        },
+        enableDebugger : function(){
+            this.options.debug = true;
+            this._debug('DEBUGGING ENABLED');
+        },
+        disableDebugger : function(){
+            this._debug('DISABLING DEBUGGER');
+            this.options.debug = false;
+        },
+        _debug : function($obj){
+            if(!this.options.debug) {return;}
+            if (window.console && window.console.log){
+                window.console.log('[%s - sub.bus]',new Date(), $obj)
             }
         }
-    },
-};
+    };
+
+})(jQuery);
